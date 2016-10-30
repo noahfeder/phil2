e1a15ce889cb08381757ba255641bffbbd113853
Noah Feder <noah.feder@gmail.com>
2016-10-30 18:03:23 -0400

    add swipe on images, add favicons, add active selectors to hovers

--- src/smart/look.js
+++ src/smart/look.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import Swipeable from 'react-swipeable';
 import store from '../index';
 import { imgLeft, imgRight } from '../actions/index';
 
@@ -14,11 +14,20 @@ class Look extends Component {
     return store.dispatch(imgRight(position));
   }
 
+  logTouch(e) {
+    console.log(this.props)
+  }
+
   images() {
     let arr = [];
     let imgOrNot = false;
     for (let i = 0; i < 12; i++) {
-      imgOrNot = ( (i >= this.props.image.current - 1 && i <= this.props.image.current + 1) || this.props.image[`position${i}`]);
+      imgOrNot = (
+        (i >= this.props.image.current - 1
+        &&
+        i <= this.props.image.current + 1)
+        ||
+        this.props.image[`position${i}`]);
       arr.push(
         <div className="col s12 look" key={ i }>
           <img src={ imgOrNot ? `img/phil${i}.jpg` : '' } alt={ `phil ${i}`} />
@@ -32,11 +41,15 @@ class Look extends Component {
     let rightClass = this.props.image.current === 11 ? 'hide' : '';
     return (
       <div className="row imageWrapper innerScroll">
-        <div className="row imageRow" style={ {
-          marginLeft: `${this.props.image.current * -100}%`
-        } }>
+        <Swipeable
+          onSwipedRight={ () => this.goLeft(this.props.image.current) }
+          onSwipedLeft={ () => this.goRight(this.props.image.current) }
+          className="row imageRow"
+          style={ {
+            marginLeft: `${this.props.image.current * -100}%`
+          } }>
           {this.images()}
-        </div>
+        </Swipeable>
         <div onClick={ () => this.goLeft(this.props.image.current) } className={ `leftArrow controls ${leftClass}` }></div>
         <div onClick={ () => this.goRight(this.props.image.current) } className={ `rightArrow controls ${rightClass}` }></div>
       </div>
